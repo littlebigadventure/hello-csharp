fe883c5964b3b6927d6f6baf72f9fafa476936b6
twinsen <sakura-tver@mail.ru>
2016-11-16 00:09:37 +0300

    Изменил класс StgreamUtil
    Добавил копирование

--- SkitExample/appStream/StreamUtil.cs
+++ SkitExample/appStream/StreamUtil.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace appStream
 {
-    class StreamUtil
+    public static class StreamUtil
     {
+        const int BufferSize = 8192;
+        public static void Copy(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[BufferSize];
+            int read;
+            while((read = input.Read(buffer, 0, buffer.Length))>0)
+            {
+                output.Write(buffer, 0, read);
+            }
+        }
     }
 }

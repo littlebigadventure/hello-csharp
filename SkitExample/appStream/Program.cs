746bbf63f9f586962ae9ae9ac1df1dc91475a139
twinsen <sakura-tver@mail.ru>
2016-11-17 14:14:43 +0300

    Demo Range
    Добавил тестовый пример с Range

--- SkitExample/appStream/Program.cs
+++ SkitExample/appStream/Program.cs
@@ -10,15 +10,30 @@ namespace appStream
 {
     class Program
     {
-        static void Main(string[] args)
+        static void DemoStream()
         {
             WebRequest request = WebRequest.Create("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0");
-            using(WebResponse response = request.GetResponse())
-            using(Stream responseStream = response.GetResponseStream())
-            using(FileStream output = File.Create("wiki.dat"))
+            using (WebResponse response = request.GetResponse())
+            using (Stream responseStream = response.GetResponseStream())
+            using (FileStream output = File.Create("wiki.dat"))
             {
                 StreamUtil.Copy(responseStream, output);
             }
+        }
+        static void DemoReverse()
+        {
+            var collection = Enumerable.Range(0, 10);
+            collection = Enumerable.Where(collection, x=> x % 2 !=0);
+            collection = Enumerable.Reverse(collection);
+            foreach(var i in collection)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DemoReverse();
+            Console.ReadLine();
         }
     }
 }
